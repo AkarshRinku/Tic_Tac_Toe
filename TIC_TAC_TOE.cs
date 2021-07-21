c9f4fcbf04d31327c7a8dd5ed4f337faa3f18852
AkarshRinku <86792553+AkarshRinku@users.noreply.github.com>
2021-07-21 12:53:17 +0530

    [akarsh] added: generating X Or O

--- TIC_TAC_TOE.cs
+++ TIC_TAC_TOE.cs
@@ -4,14 +4,46 @@ using System.Text;
 
 namespace Tic_Tac_Toe
 {
-   public class TIC_TAC_TOE
+    public class TicTacToe
     {
-        // initialising array of 9 characters
-        char[] Board = {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
-     
+        public void Board()
+        {
+            // initialising array of 9 characters
+            char[] board = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        }
+        public void play()
+        {
+            int player = 0;
+             
+            if (player==1)
+            {
+                Console.WriteLine("X is gennerated");
 
+            }
+            else
+            {
+                Console.WriteLine("O is gennerated");
 
-
-
+            }
+        }
     }
 }
+
+        
+
+
+
+
+
+
+
+
+            
+        
+       
+
+
+
+
+    
+
