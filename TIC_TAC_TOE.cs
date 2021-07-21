49d6a443773ed74a7891e1aca6b3d93743f51851
AkarshRinku <86792553+AkarshRinku@users.noreply.github.com>
2021-07-21 11:56:18 +0530

    Creating Board and assigning empty space

---
+++ TIC_TAC_TOE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tic_Tac_Toe
+{
+   public class TIC_TAC_TOE
+    {
+        // initialising array of 9 characters
+        char[] Board = {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
+        int player = '1';
+
+
+
+
+    }
+}
